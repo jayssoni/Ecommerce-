500f7644938e609c044a1c2b544e234b4a122051
jay <jaykumarsoni944@gmail.com>
2025-06-04 15:35:41 +0530

    complet without error at 5:32:00

--- controller/productcategoryCtrl.js
+++ controller/productcategoryCtrl.js
@@ -21,6 +21,43 @@ const getCategory = asyncHandler(async (req, res) => {
     }
 });
 
+const getAllCategories = asyncHandler(async (req, res) => {
+    try {
+        const categories = await Category.find({});
+        res.json(categories);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+const updateCategory = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    validateMongodbId(id);
+    try {
+        const updatedCategory = await Category.findByIdAndUpdate(id, req.body, { new: true });
+        res.json(updatedCategory);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+const deleteCategory = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    validateMongodbId(id);
+    try {
+        const deletedCategory = await Category.findByIdAndDelete(id);
+        res.json(deletedCategory);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
 
-
-module.exports={ createCategory, getCategory }
+const getaCategory = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    validateMongodbId(id);
+    try {
+        const category = await Category.findById(id);
+        res.json(category);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+module.exports={ createCategory, getCategory, getAllCategories, updateCategory, deleteCategory, getaCategory }
